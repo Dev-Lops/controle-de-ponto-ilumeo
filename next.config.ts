0efc1c08002281961f235449d73de0105b95e05b
Anderson Lopes <developesanderson@gmail.com>
2025-01-10 16:40:10 -0400

    ✅ - Fix  a api connection

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactStrictMode: true,
   pageExtensions: ["page.tsx", "api.ts", "api.tsx"],
+  compress: true, // Habilita compressão de saída
 };
 
 export default nextConfig;
