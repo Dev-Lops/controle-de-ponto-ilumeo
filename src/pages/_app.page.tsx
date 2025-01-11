6672800ab9b26e1f3e6d8cd662007821b2c2e14c
Anderson Lopes <developesanderson@gmail.com>
2025-01-11 17:06:50 -0400

    ✅ - Fix a bugs

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -4,6 +4,7 @@ import type { AppProps } from "next/app";
 import SEO from "../../next-seo.config";
 import { useState, useEffect } from "react";
 import { useRouter } from "next/router";
+import { LoadingScreen } from "@/components/LoadingScreen";
 
 
 export default function App({ Component, pageProps }: AppProps) {
