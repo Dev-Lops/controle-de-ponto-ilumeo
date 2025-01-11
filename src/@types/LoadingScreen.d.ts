26529d03dc133d2678325e38e55895393fe738ef
Anderson Lopes <developesanderson@gmail.com>
2025-01-11 17:16:07 -0400

    ✅ - Fix a 🤞

---
+++ src/@types/LoadingScreen.d.ts
@@ -0,0 +1,4 @@
+declare module "@/components/LoadingScreen" {
+  const LoadingScreen: React.FC;
+  export default LoadingScreen;
+}
--- src/components/loadingScreen.tsx
+++ src/components/loadingScreen.tsx
@@ -1,4 +1,4 @@
-export const LoadingScreen = () => {
+export default function LoadingScreen() {
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50 flex-col gap-5">
       <div className="loader">
@@ -9,4 +9,4 @@ export const LoadingScreen = () => {
       <p className="text-white mt-4 text-xl">Carregando...</p>
     </div>
   );
-};
+}
